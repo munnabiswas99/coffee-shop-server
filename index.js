5c8cc992d77113227e4db4339ef338e5e755d1d7
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-02-23 04:08:23 +0600

    C is done from CRUD

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const express = require('express')
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-
-// IHmGcDdlpubKFmfj
-// coffee_shop
-
-
-// const uri = "mongodb+srv://coffee_shop:IHmGcDdlpubKFmfj@cluster0.etxtqbz.mongodb.net/coffee_store?retryWrites=true&w=majority&appName=Cluster0";
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.etxtqbz.mongodb.net/?appName=Cluster0`;
 
@@ -23,28 +17,46 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+        // Connect to the "sample_mflix" database and access its "movies" collection
+    const database = client.db("coffeeDB");
+    const coffeCollection = database.collection("coffees");
+
+    app.get('/coffees', async(req, res) => {
+        const result = await coffeCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.post("/coffees", async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Coffee server is getting warm!')
-})
+app.get("/", (req, res) => {
+  res.send("Coffee server is getting warm!");
+});
 
 app.listen(port, () => {
-  console.log(`Coffee server is running on port ${port}`)
-})
+  console.log(`Coffee server is running on port ${port}`);
+});

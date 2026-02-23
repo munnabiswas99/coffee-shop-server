81cb717c21112521013bf135196ec782a1907133
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-02-23 16:53:35 +0600

    Delete operation done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -40,6 +40,13 @@ async function run() {
       const result = await coffeCollection.insertOne(newCoffee);
       res.send(result);
     });
+
+    app.delete('/coffees/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeCollection.deleteOne(query);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
